eaa2bca92bcd65bcb659f2902f1d3e03685a7f96
Anmol Gurung <anmolgurung1@gmail.com>
2020-07-25 13:22:13 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,17 +6,15 @@ weatherForm.addEventListener("submit", (event) => {
   event.preventDefault();
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch(`http://localhost:3000/weather?address=${search.value}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          console.log(data);
-          messageOne.textContent = data.location;
-          messageTwo.textContent = `${data.description}. Its ${data.temperature} degree here but it feels like ${data.feels_like} degree`;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${search.value}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        console.log(data);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = `${data.description}. Its ${data.temperature} degree here but it feels like ${data.feels_like} degree`;
+      }
+    });
+  });
 });
